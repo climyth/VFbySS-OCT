ee23a6e2f9b0a55f784cad4e1fc7036c2d635c62
climyth <62638658+climyth@users.noreply.github.com>
2020-04-04 19:29:26 +0900

    Update Train.py

--- Train.py
+++ Train.py
@@ -7,7 +7,7 @@ base_model_name = "InceptionV3"   # InceptionV3, InceptionV4, InceptionResnet
 base_folder = "Z:/PaperResearch/VFbySS-OCT"
 vf_file = "/train_data.xlsm"
 weight_save_folder = "/Weights/" + base_model_name
-pretrained_weights = ""   # 만약 없으면 "" 으로.
+pretrained_weights = ""   # if no pretrained weight, just leave ""
 tensorboard_log_folder = "/Logs"
 # ==================================================
 
